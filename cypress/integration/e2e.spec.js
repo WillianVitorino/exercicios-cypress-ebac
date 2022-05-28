55c964326d1bef43accf470711d3c0f8e5d89683
WillianVitorino <williancsvitorino@gmail.com>
2022-05-28 02:48:13 -0300

    exercicio módulo 12

---
+++ cypress/integration/e2e.spec.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
+    /*  Como cliente 
+        Quero acessar a Loja EBAC 
+        Para fazer um pedido de 4 produtos 
+        Fazendo a escolha dos produtos
+        Adicionando ao carrinho
+        Preenchendo todas opções no checkout
+        E validando minha compra ao final */
+
+    beforeEach(() => {
+        cy.visit('/produtos/')
+    });
+
+    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
+        cy.adicionarProduto(2, 32, 'Green', 1);
+        cy.adicionarProduto(1, 'XS', 'Yellow', 1);
+        cy.adicionarProduto(3, 'XS', 'Green', 1);
+        cy.adicionarProduto(4, 34, 'Black', 1);
+        cy.get('.dropdown-toggle > .text-skin > .icon-basket').click();
+        cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click();
+        var dados = require('../fixtures/detalhesFaturamento.json');
+        cy.detalhesFaturamento(dados.nome,dados.sobrenome, dados.nomeEmpresa, dados.pais, dados.endereco, dados.cidade, dados.Estado, dados.cep, dados.telefone, dados.email);
+        cy.get('[class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"]', {timeout: 10000}).should('contain', 'Obrigado. Seu pedido foi recebido.')
+    });
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,26 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('adicionarProduto', (posicao, tamanho, cor, quantidade)=>{
+    cy.get('[class="product-block grid"]').eq(posicao).click();
+    cy.get('.button-variable-item-'+ tamanho).click();
+    cy.get('.button-variable-item-'+ cor).click();
+    cy.get('.input-text').clear().type(quantidade);
+    cy.get('.single_add_to_cart_button').click();
+    cy.get('#primary-menu > .menu-item-629 > a').click();
+});
+
+Cypress.Commands.add('detalhesFaturamento', (nome, sobrenome, nomeEmpresa, pais, endereco, cidade, estado, cep, telefone, email) =>{
+    cy.get('#billing_first_name').type(nome);
+    cy.get('#billing_last_name').type(sobrenome);
+    cy.get('#billing_company').type(nomeEmpresa);
+    cy.get('#select2-billing_country-container').type(pais).click();
+    cy.get('#billing_address_1').type(endereco);
+    cy.get('#billing_city').type(cidade);
+    cy.get('#select2-billing_state-container').type(estado).click();
+    cy.get('#billing_postcode').type(cep);
+    cy.get('#billing_phone').type(telefone);
+    cy.get('#billing_email').type(email);
+    cy.get('#terms').check();
+    cy.get('#place_order').click();
+})
