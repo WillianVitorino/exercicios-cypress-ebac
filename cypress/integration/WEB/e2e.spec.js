24c6240990b014ff3f73a7d36d9f93055b2af4ba
willianVitorino <williancsvitorino@gmail.com>
2022-06-05 14:26:29 -0300

    Correção - Remoção dos testes de API

--- cypress/integration/WEB/e2e.spec.js
+++ cypress/integration/WEB/e2e.spec.js
@@ -20,7 +20,7 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         cy.adicionarProduto(4, 34, 'Black', 1);
         cy.get('.dropdown-toggle > .text-skin > .icon-basket').click();
         cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .checkout').click();
-        var dados = require('../fixtures/detalhesFaturamento.json');
+        var dados = require('../../fixtures/detalhesFaturamento.json');
         cy.detalhesFaturamento(dados.nome,dados.sobrenome, dados.nomeEmpresa, dados.pais, dados.endereco, dados.cidade, dados.Estado, dados.cep, dados.telefone, dados.email);
         cy.get('[class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"]', {timeout: 10000}).should('contain', 'Obrigado. Seu pedido foi recebido.')
     });
